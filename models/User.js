94534a3cd978183292ad9348eb68add471505eaa
vigansokoli <vigan.sokoli@gmail.com>
2020-10-12 16:07:02 +0200

    Models created

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name:  String, // String is shorthand for {type: String}
+  accountID: Number,
+  phone: Number,
+  email: {type: String, required: [true, "can't be blank"]},
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now }
+});
+
+
+UserSchema.path('email').validate(function (email) {
+    var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailRegex.test(email.text); // Assuming email has a text attribute
+ }, 'The e-mail field cannot be empty.')
